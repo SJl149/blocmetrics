80edfa81cc57e7711056f022d3b894b1ad7044e2
Scott Lenander <sjlteacher@gmail.com>
2017-07-03 18:22:49 -0400

    Application initial view setup and devised installed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'home/index'
 
-  get 'home/about'
+  devise_for :users
+  get 'about', to: 'home#about'
+
+  root 'home#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
